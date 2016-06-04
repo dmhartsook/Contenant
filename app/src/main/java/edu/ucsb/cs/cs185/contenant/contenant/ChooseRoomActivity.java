ed0727b1849e7ac6704d9c2b1e3db07c73bc119e
Allison Shedden <ashedden@umail.ucsb.edu>
2016-06-04 16:08:14 -0700

    Added RoomImageAdapter and UI

--- app/src/main/java/edu/ucsb/cs/cs185/contenant/contenant/ChooseRoomActivity.java
+++ app/src/main/java/edu/ucsb/cs/cs185/contenant/contenant/ChooseRoomActivity.java
@@ -4,28 +4,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
-import android.widget.Toast;
 
 /**
- * Activity for the page showing all the homes, allowing the user to choose one.
+ * Created by Allison on 6/4/2016.
  */
-public class ChooseHomeActivity extends AppCompatActivity {
+public class ChooseRoomActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.choose_home);
+        setContentView(R.layout.choose_room);
 
-        GridView gridview = (GridView) findViewById(R.id.choose_home_gridview);
-        gridview.setAdapter(new ChooseHomeImageAdapter(this));
+        GridView gridview = (GridView) findViewById(R.id.choose_room_gridview);
+        gridview.setAdapter(new ChooseRoomImageAdapter(this));
 
         gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View v,
                                     int position, long id) {
-                Intent intent = new Intent(ChooseHomeActivity.this, ViewHomeActivity.class);
+                Intent intent = new Intent(ChooseRoomActivity.this, ViewRoomActivity.class);
                 startActivity(intent);
             }
         });
